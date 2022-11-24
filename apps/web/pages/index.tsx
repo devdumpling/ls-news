2593a86ddcea1e2df4357bafb6b576e686432289
Devon Wells <devon.wellsa@gmail.com>
2022-11-23 21:15:51 -0500

    feat: basic page listing out top 10 breaking stories using fetcher

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,30 @@
-export default function Home() {
+import { fetcher, GNewsArticle } from "lsn-core";
+
+export default function Home({ articles }: { articles: GNewsArticle[] }) {
+  console.log({ articles });
+
   return (
     <div>
       <h1>lsn</h1>
       <p>list just the fucking news</p>
+      <div>
+        <h2>Breaking US Stories</h2>
+        <ul>
+          {articles.map((article) => (
+            <li key={article.title}>{article.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const data = await fetcher();
+  const { articles } = data;
+  return {
+    props: {
+      articles,
+    },
+  };
 }
