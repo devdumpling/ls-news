387f38df648a65740660f166633c4bd66efb7410
Devon Wells <devon.wellsa@gmail.com>
2022-11-24 11:56:54 -0500

    feat: vite config and ui init

--- apps/web/app/head.tsx
+++ apps/web/app/head.tsx
@@ -2,6 +2,7 @@ export default async function Head() {
   return (
     <>
       <title>ls-news | zero-bs news</title>
+      <meta name="description" content="ls-news | zero-bs news" />
     </>
   );
 }
