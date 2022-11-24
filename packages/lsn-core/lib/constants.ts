cc59b1c0475ec1655ae8a7da7bd90c7ba5213dc6
Devon Wells <devon.wellsa@gmail.com>
2022-11-23 19:11:44 -0500

    feat: constants

---
+++ packages/lsn-core/lib/constants.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const GNEWS_V4_URL = 'https://gnews.io/api/v4';
+export const GNEWS_TOKEN = process.env.GNEWS_TOKEN;
+
+export const GNEWS_URLS = {
+  breaking: `${GNEWS_V4_URL}/top-headlines?token=${GNEWS_TOKEN}&topic=breaking-news`,
+}
