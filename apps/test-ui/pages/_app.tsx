7484243fa1e306a2d4705f1943c1308d282361ca
Devon Wells <devon.wellsa@gmail.com>
2022-11-25 11:52:17 -0500

    fix: add styles to export path in ui build

--- apps/test-ui/pages/_app.tsx
+++ apps/test-ui/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "../styles/style.css"; // this works after moving the style... why isn't it just being referenced properly in the bundle?
+import "ui/styles";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
