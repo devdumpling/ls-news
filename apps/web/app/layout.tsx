793791d4a764c8e61462dd0ed1b2f325183815af
Devon Wells <devon.wellsa@gmail.com>
2022-11-26 13:13:20 -0500

    feat: clean up some styles for super MVP

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,19 +1,5 @@
 import "ui/styles.css";
 import "../styles/globals.css";
-import { Container } from "ui";
+import { RootLayout } from "ui";
 
-// TODO - swap this out for the exported Layout in the ui package when confirmed that using .css extension on import works
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Container theme="light">{children}</Container>
-      </body>
-    </html>
-  );
-}
+export default RootLayout;
--- packages/ui/lib/components/Container.css.ts
+++ packages/ui/lib/components/Container.css.ts
@@ -1,17 +1,11 @@
 import { style } from "@vanilla-extract/css";
-import { lightThemeClass, darkThemeClass } from "../theme";
 
-// TODO figure out how to make theming work
-
-export const container = style([
-  lightThemeClass,
+export const container = style([  
   {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     padding: "1rem",
-    justifyContent: "center",
     minHeight: "100vh",
-    textAlign: "center",
   },
 ]);
